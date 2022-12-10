db281e1cb68a6d672db5506eb4cd02b21d63232c
Manish Kesharwani <mk9934dude@gmail.com>
2022-12-10 10:47:30 +0000

    Doing Network calls and andling response the right way

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { uiAction } from "../../store/ui-slice";
+import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
@@ -8,7 +8,7 @@ const CartButton = (props) => {
   const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
-    dispatch(uiAction.toggle());
+    dispatch(uiActions.toggle());
   };
 
   return (
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -16,6 +16,7 @@ const ProductItem = (props) => {
         description,
       })
     );
+    
   };
 
   return (
